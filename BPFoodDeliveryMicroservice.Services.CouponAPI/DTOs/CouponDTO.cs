ad42dfbab18f6a259a4e1ab13209772ade641b36
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-04-27 12:46:24 +0530

    model dto and nuget packages

---
+++ BPFoodDeliveryMicroservice.Services.CouponAPI/DTOs/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BPFoodDeliveryMicroservice.Services.CouponAPI.DTOs;
+
+public class CouponDTO
+{
+    public int Id { get; set; }
+    public required string Code { get; set; }
+    public double Discount { get; set; }
+    public int MinAmount { get; set; }
+
+}
---
+++ BPFoodDeliveryMicroservice.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace BPFoodDeliveryMicroservice.Services.CouponAPI.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public required string Code { get; set; }
+    public double Discount { get; set; }
+    public int MinAmount { get; set; }
+
+    public DateTime LastUpdated { get; set; }
+
+}
