1f31edcc9fb6e3981f3d0cea5df9f9514ff299c0
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-05-26 13:48:48 +0530

    migration delete unused model

---
+++ BPFoodDeliveryMicroservice.AuthAPI/Migrations/20240526081640_newNameField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BPFoodDeliveryMicroservice.AuthAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class newNameField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
